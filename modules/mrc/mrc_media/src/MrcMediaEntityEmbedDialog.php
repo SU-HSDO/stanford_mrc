558753506ee3791ce53d1890b72ff2a95e5e6cab
Mike Decker <pookmish@gmail.com>
2017-11-16 09:57:50 -0800

    basic text field link support

--- modules/mrc/mrc_media/src/MrcMediaEntityEmbedDialog.php
+++ modules/mrc/mrc_media/src/MrcMediaEntityEmbedDialog.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\mrc_media;
 
+use Drupal\Component\Serialization\Json;
 use Drupal\Core\DependencyInjection\ContainerInjectionInterface;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Form\FormStateInterface;
@@ -21,6 +22,8 @@ class MrcMediaEntityEmbedDialog implements ContainerInjectionInterface {
    * @var \Drupal\Core\Entity\EntityStorageInterface
    */
   protected $editorStorage;
+
+  protected $settingsKey = 'data-entity-embed-display-settings';
 
   /**
    * Construct LinkitEntityEmbedDialog service.
@@ -48,6 +51,12 @@ class MrcMediaEntityEmbedDialog implements ContainerInjectionInterface {
    *   The current state of the form.
    */
   public function alterForm(array &$form, FormStateInterface $form_state) {
+    // Hide the image_link option since it only links to the file itself.
+    //    $form['attributes'][$this->settingsKey]['image_link']['#type'] = 'hidden';
+    //    $form['attributes'][$this->settingsKey]['image_link']['#value'] = '';
+    $form['attributes'][$this->settingsKey]['image_link']['#options']['url'] = $this->t('URL');
+    $form['attributes'][$this->settingsKey]['image_link']['#weight'] = 99;
+
     /** @var \Drupal\filter\Entity\FilterFormat $filter_format */
     $filter_format = $form_state->getBuildInfo()['args'][0];
 
@@ -62,35 +71,42 @@ class MrcMediaEntityEmbedDialog implements ContainerInjectionInterface {
 
     $linkit_profile_id = $editor->getSettings()['plugins']['drupallink']['linkit_profile'];
 
-    if (isset($form_state->getUserInput()['editor_object'])) {
-      $input = $form_state->getUserInput()['editor_object'];
-      $form_state->set('link_element', $input);
-      $form_state->setCached(TRUE);
-    }
-    else {
-      // Retrieve the link element's attributes from form state.
-      $input = $form_state->get('link_element') ?: [];
-    }
-
-    $link_form = &$form['attributes']['data-entity-embed-display-settings']['link_url'];
+    $input = [];
 
     $form['href_dirty_check'] = [
       '#type' => 'hidden',
       '#default_value' => isset($input['href']) ? $input['href'] : '',
     ];
 
-    $link_form = array_merge($link_form, [
-      '#type' => 'linkit',
+    $link_form = [
+      '#title' => $this->t('Link'),
+//      '#type' => 'linkit',
+      '#type' => 'textfield',
       '#description' => $this->t('Start typing to find content.'),
-      '#autocomplete_route_name' => 'linkit.autocomplete',
-      '#autocomplete_route_parameters' => [
-        'linkit_profile_id' => $linkit_profile_id,
-      ],
-      "#weight" => -10,
+//      '#autocomplete_route_name' => 'linkit.autocomplete',
+//      '#autocomplete_route_parameters' => [
+//        'linkit_profile_id' => $linkit_profile_id,
+//      ],
+      "#weight" => $form['attributes'][$this->settingsKey]['image_link']['#weight'] + .5,
       '#default_value' => isset($input['href']) ? $input['href'] : '',
-    ]);
+      '#element_validate' => [[self::class, 'validateElement']],
+      '#states' => [
+        'visible' => [
+          '[name="attributes[data-entity-embed-display-settings][image_link]"]' => ['value' => 'url'],
+        ],
+      ],
+    ];
 
-    $form['attributes']["#weight"] = -100;
+    $form['attributes'][$this->settingsKey]['linkit'] = $link_form;
+    array_unshift($form['#submit'], [self::class, 'submitForm']);
+  }
+
+  public static function validateElement(&$form, FormStateInterface $form_state) {
+//    dpm($form_state->getValues());
+  }
+
+  public static function submitForm(&$form, FormStateInterface $form_state) {
+//    dpm($form_state->getValues());
   }
 
 }
