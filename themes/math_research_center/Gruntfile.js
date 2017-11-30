395a5e98d2ad760476d10e622e85e332d7495cca
Joseph Knox <jgknox@stanford.edu>
2017-11-30 15:36:59 -0800

    added date-stacked component

--- themes/math_research_center/Gruntfile.js
+++ themes/math_research_center/Gruntfile.js
@@ -111,6 +111,7 @@ module.exports = function(grunt) {
           'css/components/components.css':       'scss/components/components.scss',
           // Components are compiled individually so they may be included
           // conditionally using logic built in to the theme template or module.
+          'css/components/date-stacked.css':     'scss/components/date-stacked.scss',
           'css/components/event-card.css':       'scss/components/event-card.scss',
           'css/components/news-card.css':        'scss/components/news-card.scss',
           'css/components/postcard.css':         'scss/components/postcard.scss'
