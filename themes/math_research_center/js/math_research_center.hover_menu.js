a64d869bebd03a12fd5c4c97c85f9be5ca2801da
Mike Decker <pookmish@gmail.com>
2018-01-22 15:31:45 -0800

    eliminated js except for focus

--- themes/math_research_center/js/math_research_center.hover_menu.js
+++ themes/math_research_center/js/math_research_center.hover_menu.js
@@ -2,23 +2,17 @@
   'use strict';
   Drupal.behaviors.hoverMenu = {
     attach: function (context, settings) {
-      $(".decanter-main-menu--hover-reveal li").hover(function () {
-        $(this).children("ul").slideToggle("fast");
-      });
 
-      $(".decanter-main-menu--hover-reveal li a").focus(function () {
-        $(this).parent().children("ul").slideToggle("fast");
-      }).blur(menu_lost_focus);
-
-      function menu_lost_focus(e) {
+      $(".decanter-main-menu--hover-reveal a").focus(function () {
+        $(this).siblings('ul').slideToggle("fast");
+      }).blur(function () {
         setTimeout(function () {
-          var lostParent = $(e.currentTarget).parent('li');
-          if ($(lostParent).find('a:focus').length == 0) {
-            $(lostParent).children('ul').slideToggle("fast");
+          var list = $(e.currentTarget).siblings('ul');
+          if ($(list).find('a:focus') === 0) {
+            $(list).slideToggle("fast");
           }
         }, 100);
-      }
-
+      });
     }
   };
 })(jQuery, Drupal);
