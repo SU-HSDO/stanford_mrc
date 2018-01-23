0a11a0a131a5b21278e893cc03f4fc5695dc1245
Mike Decker <pookmish@gmail.com>
2018-01-23 14:58:11 -0800

    added ability to quickly add a button that expands/collapses submenu

--- themes/math_research_center/js/math_research_center.hover_menu.js
+++ themes/math_research_center/js/math_research_center.hover_menu.js
@@ -3,6 +3,35 @@
   Drupal.behaviors.hoverMenu = {
     attach: function (context, settings) {
 
+      // Slight delay to close the menu item.
+      $('.decanter-nav-primary li').mouseout(function () {
+        var childMenu = $(this).children('ul');
+        $(childMenu).addClass('focused');
+
+        setTimeout(function () {
+          $(childMenu).removeClass('focused');
+        }, 500);
+      });
+
+      // Click/keypress to expand and collapse the menu.
+      $('button.show-hide-submenu').click(function () {
+        if ($(this).hasClass('fa-plus')) {
+          $(this).removeClass('fa-plus').addClass('fa-minus');
+          var text = $(this).find('.visually-hidden').text();
+          $(this).find('.visually-hidden').text(text.replace('Show', 'Hide'));
+          $(this).siblings('ul').addClass('focused');
+          $(this).siblings('a').attr('aria-expanded', 'true');
+        }
+        else {
+          $(this).removeClass('fa-minus').addClass('fa-plus');
+          var text = $(this).find('.visually-hidden').text();
+          $(this).find('.visually-hidden').text(text.replace('Hide', 'Show'));
+          $(this).siblings('ul').removeClass('focused');
+          $(this).siblings('a').attr('aria-expanded', 'false');
+        }
+      });
+
+
       $('.decanter-main-menu--hover-reveal a').focus(function () {
         $(this).siblings('ul').addClass('focused');
       }).blur(function (e) {
