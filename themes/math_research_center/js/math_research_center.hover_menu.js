37c3b4257663ca347cf65629522c6a03be8600a4
Mike Decker <pookmish@gmail.com>
2018-01-22 18:28:38 -0800

    focus works with tab in both directions

--- themes/math_research_center/js/math_research_center.hover_menu.js
+++ themes/math_research_center/js/math_research_center.hover_menu.js
@@ -3,16 +3,21 @@
   Drupal.behaviors.hoverMenu = {
     attach: function (context, settings) {
 
-      $(".decanter-main-menu--hover-reveal a").focus(function () {
+      $('.decanter-main-menu--hover-reveal a').focus(function () {
+        // $(this).siblings('ul').addClass('focused');
         $(this).siblings('ul').css('visibility', 'visible').css('opacity', 1);
       }).blur(function (e) {
 
+        // Need a short delay to let the focus change positions.
         setTimeout(function () {
-          var list = $(e.currentTarget).closest('ul');
-          if ($(list).find('a:focus').length === 0 && !$(list).hasClass('decanter-nav-primary')) {
-            $(list).css('visibility', 'hidden').css('opacity', 0);
-          }
-        }, 200);
+
+          $('.decanter-main-menu--hover-reveal ul.decanter-nav-submenu').each(function () {
+            if ($(this).find('a:focus').length === 0 && !$(this).siblings('a').is(':focus')) {
+              // $(this).siblings('ul').removeClass('focused');
+              $(this).css('visibility', 'hidden').css('opacity', 0);
+            }
+          })
+        }, 100);
 
       });
     }
