e8a291079c4df9991dbd1cbac8960b33da2f7e81
Mike Decker <pookmish@gmail.com>
2018-01-23 16:11:34 -0800

    used jquery ui method

--- themes/math_research_center/js/math_research_center.hover_menu.js
+++ themes/math_research_center/js/math_research_center.hover_menu.js
@@ -2,51 +2,7 @@
   'use strict';
   Drupal.behaviors.hoverMenu = {
     attach: function (context, settings) {
-
-      // Slight delay to close the menu item.
-      $('.decanter-nav-primary li').mouseout(function () {
-        var childMenu = $(this).children('ul');
-        $(childMenu).addClass('focused');
-
-        setTimeout(function () {
-          $(childMenu).removeClass('focused');
-        }, 500);
-      });
-
-      // Click/keypress to expand and collapse the menu.
-      $('button.show-hide-submenu').click(function () {
-        if ($(this).hasClass('fa-plus')) {
-          $(this).removeClass('fa-plus').addClass('fa-minus');
-          var text = $(this).find('.visually-hidden').text();
-          $(this).find('.visually-hidden').text(text.replace('Show', 'Hide'));
-          $(this).siblings('ul').addClass('focused');
-          $(this).siblings('a').attr('aria-expanded', 'true');
-        }
-        else {
-          $(this).removeClass('fa-minus').addClass('fa-plus');
-          var text = $(this).find('.visually-hidden').text();
-          $(this).find('.visually-hidden').text(text.replace('Hide', 'Show'));
-          $(this).siblings('ul').removeClass('focused');
-          $(this).siblings('a').attr('aria-expanded', 'false');
-        }
-      });
-
-
-      $('.decanter-main-menu--hover-reveal a').focus(function () {
-        $(this).siblings('ul').addClass('focused');
-      }).blur(function (e) {
-
-        // Need a short delay to let the focus change positions.
-        setTimeout(function () {
-
-          $('.decanter-main-menu--hover-reveal ul.decanter-nav-submenu').each(function () {
-            if ($(this).find('a:focus').length === 0 && !$(this).siblings('a').is(':focus')) {
-              $(this).removeClass('focused');
-            }
-          })
-        }, 10);
-
-      });
+      $('ul.decanter-nav-primary').menu();
     }
   };
 })(jQuery, Drupal);
