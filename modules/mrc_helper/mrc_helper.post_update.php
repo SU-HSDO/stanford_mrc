25ed9bb5095d91556dadedcbcd0a8fddfa941180
Mike Decker <pookmish@gmail.com>
2018-01-26 15:40:23 -0800

    changed block configurations on term pages and page content type

--- modules/mrc_helper/mrc_helper.post_update.php
+++ modules/mrc_helper/mrc_helper.post_update.php
@@ -4,38 +4,17 @@
  * mrc_helper.post_update.php
  */
 
-use Drupal\Core\Entity\Entity\EntityViewDisplay;
-
 /**
  * Adds menu block to term pages.
  */
 function mrc_helper_post_update_8_0_4() {
   \Drupal::service('module_installer')->install(['menu_block']);
 
-  $data = [
-    'config' => [
-      'provider' => 'menu_block',
-      'admin_label' => '',
-      'label' => 'Main navigation',
-      'label_display' => '0',
-      'level' => '1',
-      'depth' => '0',
-      'expand' => '1',
-      'parent' => 'main:',
-      'follow' => '1',
-      'follow_parent' => '0',
-      'suggestion' => 'main',
-    ],
-    'parent_name' => '',
-    'weight' => 0,
-    'region' => 'sidebar',
-  ];
+  module_load_install('stanford_mrc');
+  $path = drupal_get_path('module', 'mrc_helper') . '/config/install';
 
-  /** @var \Drupal\Core\Config\ConfigFactoryInterface $config_factory */
-  $config_factory = \Drupal::configFactory();
-  /** @var \Drupal\Core\Config\Config $config_entity */
-  $config_entity = $config_factory->getEditable('core.entity_view_display.taxonomy_term.mrc_event_series.default');
-  $config_entity->set('third_party_settings.mrc_ds_blocks.menu_block:main', $data);
-  $config_entity->set('third_party_settings.ds.regions.sidebar', ['menu_block:main']);
-  $config_entity->save();
+  $configs = [
+    'core.entity_view_display.taxonomy_term.mrc_event_series.default',
+  ];
+  stanford_mrc_update_configs(TRUE, $configs, $path);
 }
