e5d0da50160a4c76be7543d5c7edc170d863ff1f
Greg Garvey <ggarvey@stanford.edu>
2018-01-31 12:47:25 -0800

    MRCD8-137: added past events view and filters

--- stanford_mrc.post_update.php
+++ stanford_mrc.post_update.php
@@ -6,4 +6,8 @@
 function stanford_mrc_post_update_8_0_4() {
   $modules = ['mrc_paragraphs_webform', 'views_block_filter_block'];
   \Drupal::service('module_installer')->install($modules);
+
+  module_load_install('stanford_mrc');
+  $path = drupal_get_path('module', 'mrc_events') . '/config/install';
+  stanford_mrc_update_configs(TRUE, ['views.view.mrc_events'], $path);
 }
