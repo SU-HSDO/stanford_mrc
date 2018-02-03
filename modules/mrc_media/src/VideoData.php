e0c94a6ef957cc3c7dfb130e0f144f46d26c07e6
Greg Garvey <ggarvey@stanford.edu>
2018-02-02 18:03:46 -0800

    added service to check if video exists and adding video to media browser

---
+++ modules/mrc_media/src/VideoData.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Drupal\mrc_media;
+
+use Drupal\Core\Database\Database;
+
+/**
+* @file
+* Contains \Drupal\mrc_media\VideoData.
+*/
+
+class VideoData {
+
+  /**
+   * Constructor.
+   */
+  public function __construct() {
+
+  }
+
+  /**
+   * Checks if a video already exists in the media browser.
+   *
+   * @param $uri
+   *
+   */
+  public function videoExists($uri) {
+
+    $select = Database::getConnection()->select('media__field_media_video_embed_field', 've');
+    $select->fields('ve', array('field_media_video_embed_field_value'));
+    $select->condition('field_media_video_embed_field_value', $uri, '=');
+    $results = $select->execute();
+    return $results->fetchCol();
+  }
+
+}
