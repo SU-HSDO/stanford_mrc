f2139dbbeb2dcfcae7b9cad00c4b713091e701f4
Mike Decker <pookmish@gmail.com>
2018-02-06 14:17:15 -0800

    MRCD8-233 Changes for event date field

--- modules/mrc_events/js/mrc_events.admin.js
+++ modules/mrc_events/js/mrc_events.admin.js
@@ -4,18 +4,29 @@
   Drupal.behaviors.mrcEventsAdmin = {
     attach: function attach() {
       $('input.show-end-date[type="checkbox"]').each(function () {
+        var parent = $(this).closest('.field--type-daterange');
         if ($(this).is(':checked')) {
-          $(this).parent().prev().show().prev().show();
+          $(parent).find('.end-date').show();
         }
         else {
-          $(this).parent().prev().hide().prev().hide();
+          $(parent).find('.end-date').hide();
         }
       }).change(function () {
+        var parent = $(this).closest('.field--type-daterange');
         if ($(this).is(':checked')) {
-          $(this).parent().prev().show().prev().show();
+          var $startDate = $(parent).find('.start-date');
+          var $endDate = $(parent).find('.end-date');
+
+          $startDate.find('select').each(function () {
+            var name = $(this).attr('name').split('[').pop();
+            var value = $(this).val();
+            $endDate.find('select[name*="end_value][' + name + '"]').val(value).trigger("chosen:updated");
+          });
+
+          $endDate.show();
         }
         else {
-          $(this).parent().prev().hide().prev().hide();
+          $(parent).find('.end-date').hide();
         }
       })
     }
