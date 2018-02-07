b90b126a5a14eeddc3d5a3c5c99fe88e2bfb01a8
Greg Garvey <boznik@boznik.com>
2018-02-07 10:59:28 -0800

    Mrcd8 196 video list (#49)
    * added service to check if video exists and adding video to media browser
    
    * abstracted class for use with other media types
    
    * added view, stub for molecule and stub for adding to entity usage
    
    * cleanup
    
    * added update hook for 8.0.5 release
    
    * modified twig for display
    
    * modified twig for display
    
    * changed uri to use form api method
    
    * Create media entity on new nodes too

---
+++ modules/mrc_media/src/MediaInfo.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Drupal\mrc_media;
+
+use Drupal\Core\Database\Database;
+
+/**
+* @file
+* Contains \Drupal\mrc_media\MediaInfo.
+*/
+
+class MediaInfo {
+
+  /**
+   * Constructor.
+   */
+  public function __construct() {
+
+  }
+
+  /**
+   * Checks if a video already exists in the media browser.
+   *
+   * @param $uri
+   *
+   */
+  public function videoExists($uri) {
+
+    $select = Database::getConnection()->select('media__field_media_video_embed_field', 've');
+    $select->fields('ve', array('field_media_video_embed_field_value'));
+    $select->condition('field_media_video_embed_field_value', $uri, '=');
+    $results = $select->execute();
+    return $results->fetchCol();
+  }
+
+  /**
+   * Returns entity_id for a given uri.
+   *
+   * @param $uri
+   *
+   */
+  public function getVideoTargetId($uri) {
+    $select = Database::getConnection()->select('media__field_media_video_embed_field', 've');
+    $select->fields('ve', array('entity_id'));
+    $select->condition('field_media_video_embed_field_value', $uri, '=');
+    $results = $select->execute();
+    return $results->fetchCol();
+  }
+
+}
