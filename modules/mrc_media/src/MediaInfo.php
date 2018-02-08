f34c8321fb0a7437938bcdfd45fe8586ad276df3
Mike Decker <pookmish@gmail.com>
2018-02-08 10:48:12 -0800

    code cleanup

--- modules/mrc_media/src/MediaInfo.php
+++ modules/mrc_media/src/MediaInfo.php
@@ -5,29 +5,23 @@ namespace Drupal\mrc_media;
 use Drupal\Core\Database\Database;
 
 /**
-* @file
-* Contains \Drupal\mrc_media\MediaInfo.
-*/
-
+ * @file
+ * Contains \Drupal\mrc_media\MediaInfo.
+ */
 class MediaInfo {
-
-  /**
-   * Constructor.
-   */
-  public function __construct() {
-
-  }
 
   /**
    * Checks if a video already exists in the media browser.
    *
-   * @param $uri
+   * @param string $uri
    *
+   * @return string|null
    */
   public function videoExists($uri) {
 
-    $select = Database::getConnection()->select('media__field_media_video_embed_field', 've');
-    $select->fields('ve', array('field_media_video_embed_field_value'));
+    $select = Database::getConnection()
+      ->select('media__field_media_video_embed_field', 've');
+    $select->fields('ve', ['field_media_video_embed_field_value']);
     $select->condition('field_media_video_embed_field_value', $uri, '=');
     $results = $select->execute();
     return $results->fetchCol();
@@ -36,12 +30,14 @@ class MediaInfo {
   /**
    * Returns entity_id for a given uri.
    *
-   * @param $uri
+   * @param string $uri
    *
+   * @return string|null
    */
   public function getVideoTargetId($uri) {
-    $select = Database::getConnection()->select('media__field_media_video_embed_field', 've');
-    $select->fields('ve', array('entity_id'));
+    $select = Database::getConnection()
+      ->select('media__field_media_video_embed_field', 've');
+    $select->fields('ve', ['entity_id']);
     $select->condition('field_media_video_embed_field_value', $uri, '=');
     $results = $select->execute();
     return $results->fetchCol();
