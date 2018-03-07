fd09b1646394fb9a57addef98676da86b4fc73c5
Mike Decker <pookmish@gmail.com>
2018-03-07 14:13:17 -0800

    MRCD8-236 Added hamburger and click actions

--- themes/math_research_center/js/math_research_center.hover_menu.js
+++ themes/math_research_center/js/math_research_center.hover_menu.js
@@ -3,6 +3,14 @@
   Drupal.behaviors.mrcHoverMenu = {
     attach: function (context, settings) {
       $('#header ul.decanter-nav-primary').menu();
+
+      $('#header button.fa-bars').click(function () {
+        $(this).siblings('ul').slideToggle('slow');
+      });
+      $('#header button.fa-plus').click(function () {
+        $(this).siblings('ul').slideToggle('slow');
+        $(this).toggleClass('fa-plus').toggleClass('fa-minus');
+      })
     }
   };
 })(jQuery, Drupal);
