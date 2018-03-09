0b5b6cdf6036956a34df52aa1e1a1227afb13a1e
Mike Decker <pookmish@gmail.com>
2018-03-08 16:25:51 -0800

    added js resizing

--- themes/math_research_center/js/math_research_center.hover_menu.js
+++ themes/math_research_center/js/math_research_center.hover_menu.js
@@ -10,7 +10,18 @@
       $('#header button.fa-plus').click(function () {
         $(this).siblings('ul').toggleClass('expanded');
         $(this).toggleClass('fa-plus').toggleClass('fa-minus');
-      })
+      });
+
+      $(window).resize(function () {
+        if ($(window).width() > 600) {
+          $('#header ul').each(function () {
+            $(this).removeClass('expanded')
+          });
+          $('#header button.fa-minus').each(function () {
+            $(this).toggleClass('fa-plus').toggleClass('fa-minus');
+          })
+        }
+      });
     }
   };
 })(jQuery, Drupal);
