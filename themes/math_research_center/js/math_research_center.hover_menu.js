df95c0e3245b1fe4cd1090e1ce47a025b08853bc
Joe Knox <jgknox@stanford.edu>
2018-03-08 16:26:09 -0800

    MRCD8-236 mobile menu (#92)
    * MRCD8-236 Added hamburger and click actions
    
    * MRCD8-236 visually hidden for menu title WIP
    
    * changed toggle to classes
    
    * MRCD8-236 mobile menu styles

--- themes/math_research_center/js/math_research_center.hover_menu.js
+++ themes/math_research_center/js/math_research_center.hover_menu.js
@@ -3,6 +3,25 @@
   Drupal.behaviors.mrcHoverMenu = {
     attach: function (context, settings) {
       $('#header ul.decanter-nav-primary').menu();
+
+      $('#header button.fa-bars').click(function () {
+        $(this).siblings('ul').toggleClass('expanded');
+      });
+      $('#header button.fa-plus').click(function () {
+        $(this).siblings('ul').toggleClass('expanded');
+        $(this).toggleClass('fa-plus').toggleClass('fa-minus');
+      });
+
+      $(window).resize(function () {
+        if ($(window).width() > 600) {
+          $('#header ul').each(function () {
+            $(this).removeClass('expanded')
+          });
+          $('#header button.fa-minus').each(function () {
+            $(this).toggleClass('fa-plus').toggleClass('fa-minus');
+          })
+        }
+      });
     }
   };
 })(jQuery, Drupal);
