70138673d142b8b1b5a2d26cf5c8431e5421853a
Greg Garvey <ggarvey@stanford.edu>
2018-03-16 13:42:26 -0700

    stub for basic page changes

---
+++ modules/mrc_bricks_helper/js/mrc_bricks_helper.js
@@ -0,0 +1,9 @@
+(function ($) {
+  $(document).ready(function() {
+    var $editButtons = $('div.ief-entity-operations input.js-form-submit[value="Edit"]');
+    var editButtonCount = $editButtons.length;
+    if (editButtonCount != null && editButtonCount == 1) {
+      $editButtons.eq(0).once().mousedown();
+    }
+  });
+}(jQuery));
