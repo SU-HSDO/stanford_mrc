382cb436a58b53e10a4278321e9a11824589e6c4
Greg Garvey <ggarvey@stanford.edu>
2018-03-20 10:26:35 -0700

    commenting and cleanup

--- modules/mrc_bricks_helper/js/mrc_bricks_helper.js
+++ modules/mrc_bricks_helper/js/mrc_bricks_helper.js
@@ -1,5 +1,6 @@
 (function ($) {
   $(document).ready(function() {
+    //Open first brick if only one brick.
     var $editButtons = $('div.ief-entity-operations input.js-form-submit[value="Edit"]');
     var editButtonCount = $editButtons.length;
     if (editButtonCount != null && editButtonCount == 1) {
--- modules/mrc_page/js/mrc_page.admin.js
+++ modules/mrc_page/js/mrc_page.admin.js
@@ -1,4 +1,5 @@
 jQuery(document).ready(function($) {
+  //Confirmatioin on cancelling page changes.
   $('#edit-cancel').click(function(event){
     if(!confirm('Are you sure you want to cancel?')) {
       event.preventDefault();
