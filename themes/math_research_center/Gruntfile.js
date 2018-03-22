086748fa1c44ca116a24c55395b82500a49ad166
Joe Knox <jgknox@stanford.edu>
2018-03-22 15:17:20 -0700

    MRCD8-305 breakpoint fix for pages with subnav

--- themes/math_research_center/Gruntfile.js
+++ themes/math_research_center/Gruntfile.js
@@ -94,7 +94,7 @@ module.exports = function(grunt) {
           "node_modules/bourbon-neat/app/assets/stylesheets",
           "node_modules/font-awesome/scss",
           "node_modules/neat-omega/core",
-          "../../../../themes/custom/stanford_basic/libraries/decanter/scss",
+          "../../../../../themes/stanford/stanford_basic/libraries/decanter/scss",
           "node_modules",
         ],
         sourceMap: true,
